f8e1cea3d9b3d007e5f181f363505dc0a0607cf2
Hans Krohn <hanskrohn70@gmail.com>
2020-12-30 17:34:47 -0500

    basic nav webview

--- index.js
+++ index.js
@@ -1,50 +1,32 @@
-  let menu = false
+let menu = false
 
-  let lastScrollTop = 0
+let lastScrollTop = 0
 
-  //function to make navbar appear / disappear
-  window.addEventListener('scroll', () => {
+//function to make navbar appear / disappear
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('nav')
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop
 
-    const navbar = document.getElementById('navbar')
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+  // if(menu){
+  //   console.log('1')
+  //   navbar.style.boxShadow = 'none';
+  //   navbar.style.top = '0px';
+  // }
+  // else 
 
-    if(menu){
-      navbar.style.boxShadow = 'none';
-      navbar.style.top = '0px';
-    }
-    else if(scrollTop === 0){
-      navbar.style.boxShadow = 'none';
-      navbar.style.top = '0px';
-    }
-    else if(scrollTop > lastScrollTop){
-      navbar.style.top = '-80px';
-      navbar.style.boxShadow = 'none';
-    }
-    else{
-      navbar.style.top = '0px';
-      navbar.style.boxShadow = '0 3px 20px white';
-    }
-
-    lastScrollTop = scrollTop
-
-  })
-
-  const bringOutMenuBar = () => {
-
-    const hUL = document.getElementById('hUL')
-    const body = document.getElementById('landing')
-
-    if(menu ===  false){
-      hUL.style.left = '0%'
-      setMenu(true)
-    }
-    else if (menu === true) {
-      hUL.style.left = '-100%'
-      setMenu(false)
-    }
-
+  if(scrollTop === 0){
+    navbar.style.boxShadow = 'none';
+    navbar.style.top = '0px';
+  }
+  else if(scrollTop > lastScrollTop){
+    navbar.style.top = '-80px';
+    navbar.style.boxShadow = 'none';
+  }
+  else{
+    navbar.style.top = '0px';
+    navbar.style.boxShadow = '0px 3px 15px black';
   }
 
-  const setMenu = (value) => {
-    menu = value
-  }
+  lastScrollTop = scrollTop
+
+})
