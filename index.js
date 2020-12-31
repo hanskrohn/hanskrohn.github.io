b7c66fd3c5896f0a9a7f120f4c79fe597f812147
Hans Krohn <hanskrohn70@gmail.com>
2020-12-31 02:06:54 -0500

    typing animation

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let menu = false
-
 let lastScrollTop = 0
 
 //function to make navbar appear / disappear
@@ -8,12 +6,6 @@ window.addEventListener('scroll', () => {
   const logo = document.querySelector('.logo')
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop
 
-  // if(menu){
-  //   console.log('1')
-  //   navbar.style.boxShadow = 'none';
-  //   navbar.style.top = '0px';
-  // }
-  // else 
   console.log(scrollTop, lastScrollTop)
   if(scrollTop === 0){
     navbar.style.boxShadow = 'none';
@@ -35,5 +27,88 @@ window.addEventListener('scroll', () => {
   }
 
   lastScrollTop = scrollTop
-
 })
+
+// Init on DOM Load
+const intro = "Hi, my name is";
+const myName = "Hans Krohn";
+const description = ["Student", "Passionate Coder"];
+
+const SingleWord = function(element, word, speed){
+  this.speed = speed
+  this.element = element
+  this.word = word
+  this.txt = ''
+  this.typeStatic()
+}
+
+const WordArray = function(element, words){
+  this.element = element
+  this.words = words
+  this.txt = ''
+  this.index = 0
+  this.isDeleting = false;
+  this.typeDynamic()
+}
+
+const init = () => {
+  new SingleWord(document.getElementById('intro'), intro, 275);
+
+  setTimeout(() => {
+    new SingleWord(document.getElementById('name'), myName, 300);
+  }, 3950)
+
+  setTimeout(() => {
+    const descriptionElement = document.getElementById('description')
+    descriptionElement.style.borderRight = "2px solid #777777";
+    new WordArray(descriptionElement, description)
+  }, 6800)
+}
+
+SingleWord.prototype.typeStatic = function() {
+  const fullTxt = this.word
+
+  this.element.style.borderRight = "2px solid #777777";
+  this.txt = fullTxt.substring(0, this.txt.length + 1)
+  
+  this.element.innerHTML = this.txt
+  if(this.txt !== fullTxt){
+    setTimeout(() => this.typeStatic(), this.speed)
+  }else{
+    this.element.style.borderRight = "0px"; 
+  }
+}
+
+WordArray.prototype.typeDynamic = function() {
+  const currentIndex = this.index % this.words.length;
+
+  const fullTxt = this.words[currentIndex]
+
+  if(this.isDeleting){
+    this.txt = fullTxt.substring(0, this.txt.length - 1)
+  }else{
+    this.txt = fullTxt.substring(0, this.txt.length + 1)
+  }
+
+  this.element.innerHTML = this.txt
+  
+  let typeSpeed = 300;
+
+  
+  if(!this.isDeleting && this.txt === fullTxt){
+    console.log('waiting before deleting')
+    typeSpeed = 3000
+    this.isDeleting = true
+  }else if(this.isDeleting && this.txt === ''){
+    console.log('reseting')
+    this.isDeleting = false
+    this.index++
+    typeSpeed = 200
+  }else if(this.isDeleting){
+    typeSpeed /= 2
+  }
+  console.log(this.txt === '')
+  setTimeout(() => this.typeDynamic(), typeSpeed)
+}
+
+document.addEventListener('DOMContentLoaded', init);
