3f85fe33482fcc92f30511a9b6c945bdb8545850
Hans Krohn <hanskrohn70@gmail.com>
2020-12-31 00:14:37 -0500

    background image

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let lastScrollTop = 0
 //function to make navbar appear / disappear
 window.addEventListener('scroll', () => {
   const navbar = document.querySelector('nav')
+  const logo = document.querySelector('.logo')
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop
 
   // if(menu){
@@ -13,18 +14,24 @@ window.addEventListener('scroll', () => {
   //   navbar.style.top = '0px';
   // }
   // else 
-
+  console.log(scrollTop, lastScrollTop)
   if(scrollTop === 0){
     navbar.style.boxShadow = 'none';
     navbar.style.top = '0px';
+    navbar.style.backgroundColor = "rgba(0, 0, 0, 0.0)"
+    logo.style.display = "none"
   }
   else if(scrollTop > lastScrollTop){
     navbar.style.top = '-80px';
     navbar.style.boxShadow = 'none';
+    navbar.style.backgroundColor = "rgba(0, 0, 0, 1.0)"
+    logo.style.display = "none"
   }
   else{
     navbar.style.top = '0px';
     navbar.style.boxShadow = '0px 3px 15px black';
+    navbar.style.backgroundColor = "rgba(0, 0, 0, 1.0)"
+    logo.style.display = "inline"
   }
 
   lastScrollTop = scrollTop
