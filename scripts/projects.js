32aa09634a5da88482a50ab9a744b314af35766a
Hans Krohn <hanskrohn70@gmail.com>
2024-03-28 00:16:38 -0700

    Add forge-template

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,58 +1,79 @@
 const apps = [
   {
-    title: 'ToSBot',
-    description: 'A chrome extension that summarizes Terms of Service documents by implementing an ML model.',
-    technologies: 'Python | MongoDB | ReactTS',
+    title: "forge-template",
+    description:
+      "A CLI tool that simplifies boiler code creation. The tool is out in stable release and demo can be found on Github.",
+    technologies: "Go",
+    githubLink: "https://github.com/hanskrohn/forge-template",
+    image: "./images/forge-template.png",
+    alt: "forge-template",
+  },
+  {
+    title: "ToSBot",
+    description:
+      "A chrome extension that summarizes Terms of Service documents by implementing an ML model.",
+    technologies: "Python | MongoDB | ReactTS",
     githubLink: "https://github.com/hanskrohn/ToSBot",
     image: "./images/tosbot.png",
-    alt: "ToSBot"
+    alt: "ToSBot",
   },
   {
-    title: 'Tracker',
-    description: 'A web app I developed to help users quickly create and manage charts. Users can quickly create projects and view the data with their choice of four graph styles: Bar, Pie, Polar, and line.',
-    technologies: 'NodeJS | MongoDB | ReactJS',
+    title: "Tracker",
+    description:
+      "A web app I developed to help users quickly create and manage charts. Users can quickly create projects and view the data with their choice of four graph styles: Bar, Pie, Polar, and line.",
+    technologies: "NodeJS | MongoDB | ReactJS",
     githubLink: "https://github.com/hanskrohn/Tracker-Frontend",
     image: "./images/tracker.png",
-    alt: "Tracker"
+    alt: "Tracker",
   },
   {
-    title: 'Game Board',
-    description: 'A simple Tic Tac Toe game where users can play in a local match or against the computer. Currently, the game only has one level. Using the Minimax algorithm the computer is sure to win or tie. Don\'t belive me? Try it yourself!',
-    technologies: 'Vanilla JS | HTML | CSS',
+    title: "Game Board",
+    description:
+      "A simple Tic Tac Toe game where users can play in a local match or against the computer. Currently, the game only has one level. Using the Minimax algorithm the computer is sure to win or tie. Don't belive me? Try it yourself!",
+    technologies: "Vanilla JS | HTML | CSS",
     githubLink: "https://github.com/hanskrohn/GameBoard",
     image: "./images/GameBoard.png",
-    alt: "GameBoard"
+    alt: "GameBoard",
   },
   {
-    title: 'E-commerce',
-    description: 'A tech e-commerce site I developed. I used this project to expand on my web development knowledge. I mainly focused on the user experience, by creating visually appealing animations.',
-    technologies: 'NodeJS | MongoDB | ReactJS',
+    title: "E-commerce",
+    description:
+      "A tech e-commerce site I developed. I used this project to expand on my web development knowledge. I mainly focused on the user experience, by creating visually appealing animations.",
+    technologies: "NodeJS | MongoDB | ReactJS",
     githubLink: "https://github.com/hanskrohn/TheTechieStore-FrontEnd",
     image: "./images/eccomerce.png",
-    alt: "E-commerce"
+    alt: "E-commerce",
   },
   {
-    title: 'High or Low',
-    description: ' Web version of a the card game "Higher or Lower". Users can create rooms, and play versus their friends live.',
-    technologies: 'NodeJS | SocketIO | AngularJS',
+    title: "High or Low",
+    description:
+      ' Web version of a the card game "Higher or Lower". Users can create rooms, and play versus their friends live.',
+    technologies: "NodeJS | SocketIO | AngularJS",
     githubLink: "https://github.com/hanskrohn/HighOrLowFrontEnd",
     image: "./images/Highorlow.png",
-    alt: "High or Low"
-  }
-]
+    alt: "High or Low",
+  },
+];
 
-
-function createAppCards(){
-  let innerHTML = ''
+function createAppCards() {
+  let innerHTML = "";
 
   apps.forEach((app, i) => {
-    innerHTML += i % 2 === 0 ? template1(app) : template2(app)
-  })
+    innerHTML += i % 2 === 0 ? template1(app) : template2(app);
+  });
 
-  document.getElementById("apps").innerHTML = innerHTML
+  document.getElementById("apps").innerHTML = innerHTML;
 }
 
-function template1({title, description, technologies, appLink, githubLink, image, alt}){
+function template1({
+  title,
+  description,
+  technologies,
+  appLink,
+  githubLink,
+  image,
+  alt,
+}) {
   return `<div id = "flexDirectionColumForMobile" class = "card flex" data-aos="fade-left" data-aos-offset="200" data-aos-delay="50"  data-aos-duration="1000" data-aos-once="true">
             <div class="flex60">
               <div class = "content">
@@ -71,10 +92,18 @@ function template1({title, description, technologies, appLink, githubLink, image
                 <img src = ${image} alt = ${alt}/>
               </div>
             </div>
-          </div>`
+          </div>`;
 }
 
-function template2({title, description, technologies, appLink, githubLink, image, alt}){
+function template2({
+  title,
+  description,
+  technologies,
+  appLink,
+  githubLink,
+  image,
+  alt,
+}) {
   return `<div id = "flexDirectionColumForMobile" class = "card flex" data-aos="fade-right" data-aos-offset="200" data-aos-delay="50"  data-aos-duration="1000" data-aos-once="true">
             <div class="flex40 flex">
               <div class = "image">
@@ -93,7 +122,7 @@ function template2({title, description, technologies, appLink, githubLink, image
                 </div>
               </div>
             </div>
-          </div>`
+          </div>`;
 }
 
-createAppCards()
+createAppCards();
